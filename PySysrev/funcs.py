e597c0c9e8213508e34b3354fa633e41fe0a4b86
Thomas Luechtefeld <tomluec@gmail.com>
2018-10-24 21:14:27 -0400

    simplified getModel

--- PySysrev/funcs.py
+++ PySysrev/funcs.py
@@ -10,6 +10,8 @@ import io
 import pprint
 import boto3
 import os
+import pickle
+import urllib
 
 def getAnnotations(project_id):
     url = 'https://sysrev.com/web-api/project-annotations?project-id=' + str(project_id)
@@ -57,23 +59,5 @@ def processAnnotations(project_id, label):
 
     return final_json
 
-def getModel(model_path):
-
-    def download_dir(client, resource, dist, local='/tmp', bucket='your_bucket'):
-        paginator = client.get_paginator('list_objects')
-        for result in paginator.paginate(Bucket=bucket, Delimiter='/', Prefix=dist):
-            if result.get('CommonPrefixes') is not None:
-                for subdir in result.get('CommonPrefixes'):
-                    download_dir(client, resource, subdir.get('Prefix'), local, bucket)
-            if result.get('Contents') is not None:
-                for file in result.get('Contents'):
-                    if not os.path.exists(os.path.dirname(local + os.sep + file.get('Key'))):
-                         os.makedirs(os.path.dirname(local + os.sep + file.get('Key')))
-                    resource.meta.client.download_file(bucket, file.get('Key'), local + os.sep + file.get('Key'))
-
-    client = boto3.client('s3')
-    resource = boto3.resource('s3')
-    download_dir(client, resource, model_path, '/tmp', 'sysrev-model')
-
-    nlp = spacy.load('/tmp/' + model_path)
-    return nlp
+def getModel(model_name):
+    return pickle.loads(urllib.urlopen("https://s3.amazonaws.com/sysrev-model/".format(model_name)).read())
