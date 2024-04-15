a8c73f7acd19a396c686d75082ff790f039ced55
Thomas Luechtefeld <tom@insilica.co>
2024-04-15 09:20:33 -0400

    update github workflow. increment checkout version to 4. working on setup.py issue

---
+++ README.md
@@ -0,0 +1,14 @@
+# Sysrev Python Client
+
+The Sysrev package provides tools to interact with the sysrev.com API efficiently and programmatically. This package includes:
+
+1. **SysrevClient**: A client object for utilizing the sysrev.com API.
+2. **(Coming Soon)** A method for synchronizing local sysrev projects with their remote counterparts on sysrev.com.
+
+## Installation
+
+To install the Sysrev package, use pip:
+
+```bash
+pip install sysrev
+```
--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
 from setuptools import setup
 
-setup(name='sysrev',
-      version='1.2.2',
-      description='Gets annotations from Sysrev API',
-      url='https://github.com/sysrev/PySysrev',
-      author='Thomas Luechtefeld',
-      author_email='tom@insilica.co',
-      packages=['sysrev'],
-      install_requires=[
-          'pandas',
-          'requests',
-          'pathlib'
-      ],
-      python_requires='>=3.6',
-      zip_safe=False)
+# Assuming your README file is in Markdown
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='sysrev',
+    version='1.2.2',
+    description='get sysrev project data and use the sysrev api',
+    long_description=long_description,
+    long_description_content_type='text/markdown',  # Specify the content type here
+    url='https://github.com/sysrev/PySysrev',
+    author='Thomas Luechtefeld',
+    author_email='tom@insilica.co',
+    packages=['sysrev'],
+    install_requires=['requests'],
+    python_requires='>=3.6',
+    zip_safe=False
+)
